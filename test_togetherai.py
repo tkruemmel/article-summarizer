d8cd42ce9b2869e6361928d91437c652e69792b1
Nicole McDuffie <98335779+nemcduffie@users.noreply.github.com>
2024-09-25 16:12:50 +0200

    update generate funcs to require json

--- test_togetherai.py
+++ test_togetherai.py
@@ -1,9 +1,11 @@
+from typing import Optional
+
+from pydantic import BaseModel
 from deepeval.models.base_model import DeepEvalBaseLLM
 from deepeval.metrics import SummarizationMetric
 from deepeval.dataset import EvaluationDataset
 import json
 from together import Together
-
 
 
 # set TOGETHER_API_KEY=key
@@ -16,23 +18,65 @@ client = Together(api_key=TOGETHER_API_KEY)
 
 
 class TogetherLLM(DeepEvalBaseLLM):
-    def __init__(
-        self,
-        model
-    ):
+    def __init__(self, model):
         self.model = model
 
     def load_model(self):
         return self.model
 
-    def generate(self, prompt: str) -> str:
-        llm = client.completions.create(model=self.model, prompt=prompt)
-        return llm.choices[0].text
+    def generate(self, prompt: str, schema: BaseModel) -> BaseModel:
+        # llm = client.completions.create(model=self.model, prompt=prompt)
+        # return llm.choices[0].text
 
-    async def a_generate(self, prompt: str) -> str:
-        #llm = client.completions.create(model=self.model, prompt=prompt)
-        res = await client.completions.create(model=self.model, prompt=prompt)
-        return res.choices[0].text
+        print("----> SCHEMA: ", schema.model_json_schema())  # TODO: remove
+
+        llm = client.chat.completions.create(
+            model=self.model,
+            messages=[
+                {
+                    "role": "system",
+                    "content": "The following are questions about a news article. Only answer in JSON.",
+                },
+                {
+                    "role": "user",
+                    "content": prompt,
+                },
+            ],
+            response_format={
+                "type": "json_object",
+                "schema": schema.model_json_schema(),
+            },
+        )
+
+        print("----> OUTPUT: ", llm.choices[0].message.content)  # TODO: remove
+
+        json_result = json.loads(llm.choices[0].message.content)
+        return schema(**json_result)
+
+    async def a_generate(self, prompt: str, schema: BaseModel) -> BaseModel:
+        # llm = client.completions.create(model=self.model, prompt=prompt)
+        # res = await client.completions.create(model=self.model, prompt=prompt)
+        # return res.choices[0].text
+        res = await client.chat.completions.create(
+            model=self.model,
+            messages=[
+                {
+                    "role": "system",
+                    "content": "The following are questions about a news article. Only answer in JSON.",
+                },
+                {
+                    "role": "user",
+                    "content": prompt,
+                },
+            ],
+            response_format={
+                "type": "json_object",
+                "schema": schema.model_json_schema(),
+            },
+        )
+
+        json_result = json.loads(res.choices[0].message.content)
+        return schema(**json_result)
 
     def get_model_name(self):
         return "Custom together.ai LLM"
@@ -40,19 +84,26 @@ class TogetherLLM(DeepEvalBaseLLM):
 
 custom_model = TogetherLLM(model="mistralai/Mixtral-8x7B-Instruct-v0.1")
 
-metric = SummarizationMetric(threshold=0.5, model=custom_model, async_mode=False)
+metric = SummarizationMetric(
+    threshold=0.5, model=custom_model, async_mode=False
+)
 
 dataset = EvaluationDataset()
 dataset.add_test_cases_from_json_file(
     file_path="data.json",
     input_key_name="content",
-    actual_output_key_name="summary")
+    actual_output_key_name="summary",
+)
 
 evaluation_results = []
 
 for test_case in dataset.test_cases:
     metric.measure(test_case)
-    score_dict = {"test_case":test_case, "score":metric.score, "reason":metric.reason}
+    score_dict = {
+        "test_case": test_case,
+        "score": metric.score,
+        "reason": metric.reason,
+    }
     evaluation_results.append(score_dict)
     print(metric.score)
     print(metric.reason)
